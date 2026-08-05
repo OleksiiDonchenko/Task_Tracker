a97fd1cb0dd17d5430f808f10831552b6f840278
Oleksii Donchenko <alexeyip68@gmail.com>
2026-08-05 22:14:22 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import MainBoard from './components/MainBoard/MainBoard'
+import Sidebar from './components/Sidebar/Sidebar'
+
+function App() {
+
+  return (
+    <>
+      <Sidebar />
+      <MainBoard />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MainBoard/MainBoard.tsx
@@ -0,0 +1,12 @@
+import { } from 'react';
+import styles from './MainBoard.module.css';
+
+const MainBoard = () => {
+  return (
+    <main className={styles.mainboard}>
+      MainBoard
+    </main>
+  );
+};
+
+export default MainBoard;
