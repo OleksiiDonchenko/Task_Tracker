ed4d53ad7f4527987005a518b2189352885abaf3
Oleksii Donchenko <alexeyip68@gmail.com>
2026-08-10 16:43:20 +0300

    Style: add ThemeToggle component

--- src/components/MainBoard/ThemeToggle/ThemeToggle.tsx
+++ src/components/MainBoard/ThemeToggle/ThemeToggle.tsx
@@ -1,10 +1,29 @@
-import { } from 'react';
+import { useState } from 'react';
+import styles from './ThemeToggle.module.css';
+import { MoonIcon, SunIcon } from '../../../assets/icons/components';
+
+type Theme = 'light' | 'dark';
 
 const ThemeToggle = () => {
-  return (
-    <div>
+  const [theme, setTheme] = useState<Theme>('light');
 
-    </div>
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === 'light' ? 'dark' : 'light'));
+  };
+
+  return (
+    <div className={styles.themeToggle}
+      onClick={toggleTheme}
+      data-theme={theme}
+      role="button"
+      tabIndex={0}>
+      <div className={styles.iconWrapper}>
+        <SunIcon />
+      </div>
+      <div className={styles.iconWrapper}>
+        <MoonIcon />
+      </div>
+    </div >
   );
 };
 
