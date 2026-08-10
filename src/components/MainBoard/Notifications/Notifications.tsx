94913ab1cb32f5dfeab48b7765b4997e92afc811
Oleksii Donchenko <alexeyip68@gmail.com>
2026-08-10 21:40:33 +0300

    Style: add Notifications component

--- src/components/MainBoard/Notifications/Notifications.tsx
+++ src/components/MainBoard/Notifications/Notifications.tsx
@@ -1,9 +1,11 @@
 import { } from 'react';
+import styles from './Notifications.module.css';
+import { BellIcon } from '../../../assets/icons/components';
 
 const Notifications = () => {
   return (
-    <div>
-      
+    <div className={styles.notifications} role='button' tabIndex={0}>
+      <BellIcon />
     </div>
   );
 };
