dc058dd9e3824eb8c95cd8ac95b4584564837919
Oleksii Donchenko <alexeyip68@gmail.com>
2026-08-11 15:57:15 +0300

    Style: add UserProfile component

--- src/components/MainBoard/UserProfile/UserProfile.tsx
+++ src/components/MainBoard/UserProfile/UserProfile.tsx
@@ -1,9 +1,42 @@
-import { } from 'react';
+import { useState } from 'react';
+import styles from './UserProfile.module.css';
+import blankUserProfilePhoto from '../../../assets/blank-profile.png';
+import { ArrowDownIcon, ArrowUpIcon } from '../../../assets/icons/components';
+
+type ArrowStatus = 'closed' | 'open';
 
 const UserProfile = () => {
+  const blankUser = {
+    photo: blankUserProfilePhoto,
+    name: 'First',
+    surname: 'Second',
+  };
+
+  let user = blankUser;
+
+  const [arrowStatus, setArrowStatus] = useState<ArrowStatus>('closed');
+
+  const toggleArrow = () => {
+    setArrowStatus(prev => prev === 'closed' ? 'open' : 'closed');
+  }
+
   return (
-    <div>
-      
+    <div className={styles.userProfile}>
+      <img src={user.photo} alt='user picture' className={styles.userPhoto} />
+      <div className={styles.name}>{`${user.name} ${user.surname}`}</div>
+      <div
+        className={styles.arrow}
+        onClick={toggleArrow}
+        data-arrow={arrowStatus}
+        role='button'
+        tabIndex={0}>
+        <div className={styles.arrowWrapper}>
+          <ArrowDownIcon />
+        </div>
+        <div className={styles.arrowWrapper}>
+          <ArrowUpIcon />
+        </div>
+      </div>
     </div>
   );
 };
